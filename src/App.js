f4a38cfedb33c175da5907bcd54dd2a8d349481b
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2021-06-06 00:44:56 +0530

    Home Section 1

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="section">
       <header className="App-header">
         <img src="./assets/jumbotron.png" className="App-logo" alt="logo" />
       </header>
+      <div className="Title">
+        <h2 className="heading">HACKING HEIST</h2>
+        <h4 className="Description">Register for the Hackathon and Unlock your path to the “King of the Ocean”</h4>
+      <div className="Buttons">
+          <a href="" target="_blank"><div className="Button1">Register Now</div></a>
+          <a href="" target="_blank"><div className="Button2">Join Discord</div></a>
+      </div>
+      </div>
+      </div>
     </div>
   );
 }
