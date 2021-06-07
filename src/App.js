b762d0b94317e7d3346b78c422ad59750d9f5632
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2021-06-07 19:31:57 +0530

    Home Page Components Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import Jumbotron from './components/Home/Jumbotron/Jumbotron'
 import AboutUs from './components/Home/AboutUs/AboutUs'
+import Registration from './components/Home/Registration/Registration'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       
       <div className="section">
         <AboutUs/>
+        <Registration/>
       </div>
 
     </div>
