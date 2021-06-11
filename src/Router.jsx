6d8463033d023f432292c38e02c1ff1b0c0f96ba
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2021-06-12 00:07:45 +0530

    Team Page Base Added

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as BRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home'
+import Team from './pages/Team/Team'
 
 export default function Router() {
     return (
        <BRouter>
             <Switch>
                 <Route path='/' exact component={Home} />
+                <Route path='/team' exact component={Team} />
             </Switch>
         </BRouter>
     )
---
+++ src/pages/Team/Team.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+
+export default function Team() {
+    return (
+        <div className='team'>
+            <Navbar/>
+            <Footer/>
+        </div>
+    )
+}
