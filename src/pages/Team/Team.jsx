ddab0347fae486b084e3b99579b13b1fd2b88b27
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2021-06-12 00:29:39 +0530

    Team Page Update

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import './style.scss'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 
 export default function Team() {
     return (
+        <>
+        <Navbar/>
         <div className='team'>
-            <Navbar/>
-            <Footer/>
+                <div className='team-section'>
+                    <h1 className='heading'>Our Team</h1>
+                    <h2 className='subHeading'>Meet the spirited team that worked behind the scenes for Hacking Heist’s Succss.</h2>
+                </div>
         </div>
+        <Footer/>
+        </>
     )
 }
