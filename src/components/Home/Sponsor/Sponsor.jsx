1cbf725bdddba6770f19007437cd095d83dde44d
heet-kumar <heetkumarpatel@gmail.com>
2021-07-14 00:13:25 +0530

    Sponsors updated

--- src/components/Home/Sponsor/Sponsor.jsx
+++ src/components/Home/Sponsor/Sponsor.jsx
@@ -6,41 +6,59 @@ export default function Sponsor() {
         <div className='sponsor'>
             <h1 className='title'>Sponsors</h1>
             
-            <div className='sponsor-category'>
+            {/* <div className='sponsor-category'>
                 <h2 className='category-title'>Captain Sponsor</h2>
                 <div className='sponsor-grid'>
-                    <span className='sponsor'><img src='./assets/coil.png' width='90%' height='80%' alt=''></img></span>
-                    <span className='sponsor'><img src='./assets/qoom.png' width='90%' height='auto' alt=''></img></span>
-                    {/* <span className='sponsor'><img src='./assets/amazon.png' width='90%' height='auto' alt=''></img></span> */}
+                    <span className='sponsorc'><img src='./assets/coil.png' width='90%' height='70%' alt=''></img></span>
+                    <span className='sponsorc'><img src='./assets/qoom.png' width='90%' height='auto' alt=''></img></span>
+                    <span className='sponsorc'><img src='./assets/amazon.png' width='90%' height='auto' alt=''></img></span>
                 </div>
             </div>
 
-            {/* <div className='sponsor-category'>
+            <div className='sponsor-category'>
                 <h2 className='category-title'>Quarter Master Sponsor</h2>
                 <div className='sponsor-grid'>
-                    <span className='sponsor'><img src='./assets/facebook.png' width='100%' height='auto' alt=''></img></span>
-                    <span className='sponsor'><img src='./assets/google.png' width='100%' height='auto' alt=''></img></span>
-                    <span className='sponsor'><img src='./assets/amazon.png' width='100%' height='auto' alt=''></img></span>
+                    <span className='sponsorc'><img src='./assets/facebook.png' width='100%' height='auto' alt=''></img></span>
+                    <span className='sponsorc'><img src='./assets/google.png' width='100%' height='auto' alt=''></img></span>
+                    <span className='sponsorc'><img src='./assets/amazon.png' width='100%' height='auto' alt=''></img></span>
                 </div>
             </div>
 
             <div className='sponsor-category'>
                 <h2 className='category-title'>Sailing Master Sponsor</h2>
                 <div className='sponsor-grid'>
-                    <span className='sponsor'><img src='./assets/facebook.png' width='90%' height='auto' alt=''></img></span>
-                    <span className='sponsor'><img src='./assets/google.png' width='90%' height='auto' alt=''></img></span>
-                    <span className='sponsor'><img src='./assets/amazon.png' width='90%' height='auto' alt=''></img></span>
+                    <span className='sponsorc'><img src='./assets/facebook.png' width='90%' height='auto' alt=''></img></span>
+                    <span className='sponsorc'><img src='./assets/google.png' width='90%' height='auto' alt=''></img></span>
+                    <span className='sponsorc'><img src='./assets/amazon.png' width='90%' height='auto' alt=''></img></span>
                 </div>
-            </div>
+            </div> */}
 
             <div className='sponsor-category'>
                 <h2 className='category-title'>Gunner Sponsor</h2>
                 <div className='sponsor-grid'>
-                    <span className='sponsor'><img src='./assets/facebook.png' width='90%' height='auto' alt=''></img></span>
-                    <span className='sponsor'><img src='./assets/google.png' width='90%' height='auto' alt=''></img></span>
-                    <span className='sponsor'><img src='./assets/amazon.png' width='90%' height='auto' alt=''></img></span>
+                    <a href="https://coil.com/" target="_blank" className='sponsorc'>
+                        <img src='./assets/coil.png' width='80%' height='70%' alt=''></img>
+                    </a>
+                    <a href="https://www.qoom.io/" target="_blank" className='sponsorc'>
+                        <img src='./assets/qoom.png' width='80%' height='auto' alt=''></img>
+                    </a>
+                    {/* <span className='sponsorc'><img src='./assets/amazon.png' width='90%' height='auto' alt=''></img></span> */}
                 </div>
-            </div> */}
+            </div>
+
+            <div className='sponsor-category'>
+                <h2 className='category-title'>Authentication Partner</h2>
+                <div className='sponsor-grid1'>
+                    <a href="https://sawolabs.com/?utm_source=hacakthon&utm_medium=DefHack&utm_campaign=HackPartner" target="_blank" className='sponsorc'>
+                        <img src='./assets/sawo labs.png' className="sawolabs" alt=''></img>
+                    </a>
+                    {/* <a href="" className='sponsorc'>
+                        <img src='./assets/qoom.png' width='80%' height='auto' alt=''></img>
+                    </a> */}
+                    {/* <span className='sponsorc'><img src='./assets/amazon.png' width='90%' height='auto' alt=''></img></span> */}
+                </div>
+            </div>
+
         </div>
     )
 }
