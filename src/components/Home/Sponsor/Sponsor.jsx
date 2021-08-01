948acaae09baa63d4a619b826e270d990c37c704
heet-kumar <heetkumarpatel@gmail.com>
2021-08-01 16:36:57 +0530

    Captain Sponser Added

--- src/components/Home/Sponsor/Sponsor.jsx
+++ src/components/Home/Sponsor/Sponsor.jsx
@@ -7,11 +7,17 @@ export default function Sponsor() {
             <h1 className='title'>Sponsors</h1>
             
             <div className='sponsor-category'>
-                <h2 className='category-title'>Captain Sponsor</h2>
-                <div className='sponsor-grid'>
-                    <span className='sponsorc'><img src='./assets/coil.png' width='90%' height='70%' alt=''></img></span>
-                    <span className='sponsorc'><img src='./assets/qoom.png' width='90%' height='auto' alt=''></img></span>
-                    <span className='sponsorc'><img src='./assets/amazon.png' width='90%' height='auto' alt=''></img></span>
+                <h2 className='category-title'>Captain</h2>
+                <div className='sponsor-grid1'>
+                    <div className="ssize">
+                        <a className='sponsorc' href="https://sawolabs.com/?utm_source=hacakthon&utm_medium=DefHack&utm_campaign=HackPartner" target="_blank" >
+                            <img src='./assets/datastax.png' width='70%' height='auto' alt=''></img>
+                        </a>
+                    </div>
+                    {/* <a href="" className='sponsorc'>
+                        <img src='./assets/qoom.png' width='80%' height='auto' alt=''></img>
+                    </a> */}
+                    {/* <span className='sponsorc'><img src='./assets/amazon.png' width='90%' height='auto' alt=''></img></span> */}
                 </div>
             </div>
 
@@ -40,7 +46,7 @@ export default function Sponsor() {
                         <img src='./assets/coilnew.png' width='80%' height='auto' alt=''></img>
                     </a>
                     <a href="https://symbl.ai/" target="_blank" className='sponsorc'>
-                        <img className="filter" src='./assets/symbl1.png' width='80%' height='120%' alt=''></img>
+                        <img className="filter" src='./assets/symbl1.png' width='80%' height='110%' alt=''></img>
                     </a>
                     <a href="https://www.qoom.io/" target="_blank" className='sponsorc'>
                         <img src='./assets/qoom.png' width='80%' height='auto' alt=''></img>
