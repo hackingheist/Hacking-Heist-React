993694bface5c827deb831a74e5c280d3e5efc24
heetkumar <heetkumarpatel@gmail.com>
2021-09-17 13:55:39 +0530

    Community Partners and Sponsors are updated

--- src/components/Home/Sponsor/Sponsor.jsx
+++ src/components/Home/Sponsor/Sponsor.jsx
@@ -27,20 +27,11 @@ export default function Sponsor() {
             <div className='sponsor-category'>
                 <h2 className='category-title'>Quarter Master</h2>
                 <div className='sponsor-grid2'>
-                    <span className='sponsorc'><img className="logosmall" src='https://s3.amazonaws.com/challengepost/sponsors/logos/000/022/150/highres/Tabnine_Logo_Regular_%281%29.png' width='100%' height='auto' alt=''></img></span>
-                    <span className='sponsorc'><img className="logosmall" src='https://s3.amazonaws.com/challengepost/sponsors/logos/000/022/156/highres/InspiritAI_Logo.png' width='100%' height='auto' alt=''></img></span>
+                    <a className='sponsorc'><img className="logosmall" src='https://s3.amazonaws.com/challengepost/sponsors/logos/000/022/150/highres/Tabnine_Logo_Regular_%281%29.png' width='100%' height='auto' alt=''></img></a>
+                    <a className='sponsorc'><img className="logosmall" src='https://s3.amazonaws.com/challengepost/sponsors/logos/000/022/156/highres/InspiritAI_Logo.png' width='100%' height='auto' alt=''></img></a>
                     {/* <span className='sponsorc'><img src='./assets/amazon.png' width='100%' height='auto' alt=''></img></span> */}
                 </div>
             </div>
-
-            {/* <div className='sponsor-category'>
-                <h2 className='category-title'>Gunner Sponsor</h2>
-                <div className='sponsor-grid'>
-                    <span className='sponsorc'><img src='./assets/facebook.png' width='90%' height='auto' alt=''></img></span>
-                    <span className='sponsorc'><img src='./assets/google.png' width='90%' height='auto' alt=''></img></span>
-                    <span className='sponsorc'><img src='./assets/amazon.png' width='90%' height='auto' alt=''></img></span>
-                </div>
-            </div> */}
 
             <div className='sponsor-category'>
                 <h2 className='category-title'>Sailing Master</h2>
@@ -66,9 +57,9 @@ export default function Sponsor() {
             <div className='sponsor-category'>
                 <h2 className='category-title'>Gunner</h2>
                 <div className='sponsor-grid3'>
-                    <span className='sponsorc'><img className="logosmall" src='https://s3.amazonaws.com/challengepost/sponsors/logos/000/022/151/highres/StreamYard_-_Horz_%282%29.png'  alt=''></img></span>
-                    <span className='sponsorc'><img className="filter logosmall" src='https://s3.amazonaws.com/challengepost/sponsors/logos/000/022/155/highres/repl.png'  alt=''></img></span>
-                    <span className='sponsorc'><img src='https://raw.githubusercontent.com/hackingheist/Hacking-Heist-React/master/pictures/Sponsors/6.png' width='100%' height='auto' alt=''></img></span>
+                    <a className='sponsorc'><img className="logosmall" src='https://s3.amazonaws.com/challengepost/sponsors/logos/000/022/151/highres/StreamYard_-_Horz_%282%29.png'  alt=''></img></a>
+                    <a className='sponsorc'><img className="filter logosmall" src='https://s3.amazonaws.com/challengepost/sponsors/logos/000/022/155/highres/repl.png'  alt=''></img></a>
+                    <a className='sponsorc'><img className="logosmall" src='https://raw.githubusercontent.com/hackingheist/Hacking-Heist-React/master/pictures/Sponsors/6.jfif' alt=''></img></a>
                 </div>
             </div>
 
